338fa8d20a4bd4e50c4c9ca7f2d11736738d2846
Gerard Schornagel <GerardSchornagel@users.noreply.github.com>
2020-12-15 06:39:35 +0100

    Day 06: Part 2

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -57,6 +57,104 @@ namespace Day06
                 Console.WriteLine(answerPart1.ToString()); //6351
             }
 
+            //--PART 02 BEGIN
+            {
+                int answerPart2 = 0;
+                int[] groupCommons = new int[input.Length];
+                int i = 0;
+
+                foreach (string group in input)
+                {
+                    string[] groupIndividual = group.Split("\r\n");
+                    int groupSize = groupIndividual.Length;
+                    groupCommons[i] = 0;
+
+                    decimal occA = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("a")) { occA++; } }
+                    occA = Math.Floor(occA / groupSize);
+                    decimal occB = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("b")) { occB++; } }
+                    occB = Math.Floor(occB / groupSize);
+                    decimal occC = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("c")) { occC++; } }
+                    occC = Math.Floor(occC / groupSize);
+                    decimal occD = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("d")) { occD++; } }
+                    occD = Math.Floor(occD / groupSize);
+                    decimal occE = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("e")) { occE++; } }
+                    occE = Math.Floor(occE / groupSize);
+                    decimal occF = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("f")) { occF++; } }
+                    occF = Math.Floor(occF / groupSize);
+                    decimal occG = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("g")) { occG++; } }
+                    occG = Math.Floor(occG / groupSize);
+                    decimal occH = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("h")) { occH++; } }
+                    occH = Math.Floor(occH / groupSize);
+                    decimal occI = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("i")) { occI++; } }
+                    occI = Math.Floor(occI / groupSize);
+                    decimal occJ = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("j")) { occJ++; } }
+                    occJ = Math.Floor(occJ / groupSize);
+                    decimal occK = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("k")) { occK++; } }
+                    occK = Math.Floor(occK / groupSize);
+                    decimal occL = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("l")) { occL++; } }
+                    occL = Math.Floor(occL / groupSize);
+                    decimal occM = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("m")) { occM++; } }
+                    occM = Math.Floor(occM / groupSize);
+                    decimal occN = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("n")) { occN++; } }
+                    occN = Math.Floor(occN / groupSize);
+                    decimal occO = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("o")) { occO++; } }
+                    occO = Math.Floor(occO / groupSize);
+                    decimal occP = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("p")) { occP++; } }
+                    occP = Math.Floor(occP / groupSize);
+                    decimal occQ = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("q")) { occQ++; } }
+                    occQ = Math.Floor(occQ / groupSize);
+                    decimal occR = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("r")) { occR++; } }
+                    occR = Math.Floor(occR / groupSize);
+                    decimal occS = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("s")) { occS++; } }
+                    occS = Math.Floor(occS / groupSize);
+                    decimal occT = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("t")) { occT++; } }
+                    occT = Math.Floor(occT / groupSize);
+                    decimal occU = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("u")) { occU++; } }
+                    occU = Math.Floor(occU / groupSize);
+                    decimal occV = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("v")) { occV++; } }
+                    occV = Math.Floor(occV / groupSize);
+                    decimal occW = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("w")) { occW++; } }
+                    occW = Math.Floor(occW / groupSize);
+                    decimal occX = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("x")) { occX++; } }
+                    occX = Math.Floor(occX / groupSize);
+                    decimal occY = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("y")) { occY++; } }
+                    occY = Math.Floor(occY / groupSize);
+                    decimal occZ = 0;
+                    foreach (string question in groupIndividual) { if (question.Contains("z")) { occZ++; } }
+                    occZ = Math.Floor(occZ / groupSize);
+
+                    decimal occTotal = (occA + occB + occC + occD + occE + occF + occG + occH + occI + occJ + occK + occL + occM + occN + occO + occP + occQ + occR + occS + occT + occU + occV + occW + occX + occY + occZ);
+                    answerPart2 += int.Parse(occTotal.ToString());
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Part 02: " + answerPart2.ToString()); //3143
+            }
         }
     }
 }
