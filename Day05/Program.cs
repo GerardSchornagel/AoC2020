65da82ab89fb272baf68fefd26135d5ced2f413a
Gerard Schornagel <GerardSchornagel@users.noreply.github.com>
2020-12-15 03:56:14 +0100

    Day 05: Part 2

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -49,6 +49,37 @@ namespace Day05
                 }
                 Console.WriteLine(answerPart1.ToString()); //935
             }
+
+            //--PART 02
+            {
+                List<int> answerList = new List<int>();
+                i = boardingpasses.Length / 3;
+                i--;
+                while (i > 0)
+                {
+                    answerList.Add(boardingpasses[i, 2]);
+                    i--;
+                }
+
+                answerList.Sort();
+                i = answerList.Count-1;
+                answerList.Add(0); // Ugly way to prevent index out of range in next While-loop (1st iteration)
+                while (i > 0)
+                {
+                    if ((answerList[i] - 1) != (answerList[i - 1]))
+                    {
+                        Console.WriteLine("----------------------");
+                        Console.WriteLine("Nex2: " + answerList[i - 2]);
+                        Console.WriteLine("Next: " + answerList[i - 1]);
+                        Console.WriteLine("Curr: " + answerList[i]);
+                        Console.WriteLine("Prev: " + answerList[i + 1]);
+                    }
+                    else { }
+                    i--;
+                }
+                // Strangely I got returned two missing seats, 513 & 743.
+                // 743 was the answer though.
+            }
         }
     }
 }
